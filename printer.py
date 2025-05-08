f82c91e75460b5943060342dabdb3f7c3399eae2
Leon Skinner <Leon.skinner@bt.com>
2025-05-08 08:39:50 +0100

    undo

--- printer.py
+++ printer.py
@@ -30,20 +30,11 @@ def format_receipt(payload):
     data = json.loads(payload)
     p = Usb(0x04b8, 0x0202)  # Vendor & Product ID for Epson TM-T70
     
-    # Store name
-    p.set(align='center', bold=True, height=3, width=3)
-    p.text("Mobile Order\n")
-
+    # Store name (centered & bold)
     p.set(align='center', bold=True, height=2, width=2)
     p.text(data['order_details']['store'] + "\n")
     p.set(bold=False, height=1, width=1)
     p.text("\n")
-
-    p.set(align='center', bold=True, height=4, width=4)
-    p.text(data['order_details']['store_address'] + "\n")
-    p.set(bold=False, height=1, width=1)
-    p.text("\n")
-    p.text("\u2500" * 32 + "\n")
     
     # Order ID
     p.set(align='left')
@@ -59,7 +50,7 @@ def format_receipt(payload):
       name = item['name']
       quantity = 1
       price = float(item['price'])
-      p.text(f"{name} x{quantity} @ £{price:.2f}\n")
+      p.text(f"{name} x{quantity} @ ${price:.2f}\n")
       for modifier in item['modifiers']:
         p.text(f" + {modifier['name']} {modifier['ingredient_group']}\n")
 
@@ -68,22 +59,17 @@ def format_receipt(payload):
     p.set(align='left', bold=False, height=1, width=1)
 
     # Total
-    total = float(data['total'])
+    total = float(data['total']
     p.text("\n")
     p.set(bold=True)
-    p.text(f"Total: £{total:.2f}\n")
+    p.text(f"Total: ${total:.2f}\n")
     p.set(bold=False)
     p.text("\n")
     
     # Order details
-    p.set(font='b')
     p.text(f"Started at: {data['order_details']['started_at_time']}\n")
     p.text(f"Last updated at: {data['order_details']['last_updated_at_time']}\n")
-    p.set(font='a')
-
-    order_id = str(data["order_id"]).zfill(9)
-    p.barcode(order_id, 'CODE39', width=2, height=100, position='BELOW', align_ct=True)
-
+    
     # Cut paper
     p.cut()
     
